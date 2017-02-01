f66ceecd6b74419eede9c3ab6eee2ec72394bcb3
MicahCastillo <mc0840756@otc.edu>
2017-02-01 12:52:27 -0600

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,27 +1,15 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-
-namespace ch2_formatting
+namespace ch4_ex_104_student
 {
-    /// <summary>
-    /// Class:      program
-    /// Developer:  Micah Castillo
-    /// Date:       1/25/2017
-    /// Purpose:    use C# formatting
-    /// </summary>
     class Program
     {
-        /// <summary>
-        /// The main
-        /// </summary>
-        /// <param name="args"></param>
         static void Main(string[] args)
         {
-            double price = 1089.30;
-            Console.WriteLine("the price is {0:C}", price);
-            Console.WriteLine("negative two as currency {0:C}", -2);
-            Console.WriteLine("the price as fixed {0:F0}" ,price);
-            Console.Read();
         }
     }
 }
---
+++ student.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ch4_ex_104_student
+{
+    public class student
+    {
+        private string major;
+        private int score1;
+        private int score2;
+        private int score3;
+        private string studentfirstname;
+        private string studentlastname;
+        private string studentnumber;
+
+        public student()
+        {
+
+        }
+        
+        public student(string sid)
+        {
+            studentnumber = sid;
+        }
+        
+        public student(string sid, string lastname, string firstname)
+        {
+            studentnumber = sid;
+            studentfirstname = firstname;
+            studentlastname = lastname;
+        }
+
+        public student(string sid, string lastname, string firstname, int s1, int s2, int s3, string maj)
+        {
+            studentnumber = sid;
+            studentfirstname = firstname;
+            studentlastname = lastname;
+            score1 = s1;
+            score2 = s2;
+            score3 = s3;
+            major = maj;
+
+        }
+    }
+}
