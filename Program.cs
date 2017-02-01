759932ae7e66ca311cddd68a8d09594296d6a600
MicahCastillo <mc0840756@otc.edu>
2017-02-01 12:49:43 -0600

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+
+namespace ch2_formatting
+{
+    /// <summary>
+    /// Class:      program
+    /// Developer:  Micah Castillo
+    /// Date:       1/25/2017
+    /// Purpose:    use C# formatting
+    /// </summary>
+    class Program
+    {
+        /// <summary>
+        /// The main
+        /// </summary>
+        /// <param name="args"></param>
+        static void Main(string[] args)
+        {
+            double price = 1089.30;
+            Console.WriteLine("the price is {0:C}", price);
+            Console.WriteLine("negative two as currency {0:C}", -2);
+            Console.WriteLine("the price as fixed {0:F0}" ,price);
+            Console.Read();
+        }
+    }
+}
